24e110529a3b90a03703d28a859755715f1ca135
Nathan Hand <administrator@thehiddennation.com>
2014-05-05 18:57:13 +0100

    init base

---
+++ core/functions.php
@@ -0,0 +1,19 @@
+<?PHP
+/**
+Functions
+**/
+function Navigation()
+{
+	echo "<div class='masthead clearfix'>
+            <div class='inner'>
+              <h3 class='masthead-brand'>Nathan Hand</h3>
+              <ul class='nav masthead-nav'>
+                <li class='active'><a href='index.php'>Home</a></li>
+                <li><a href='./projects/'>My Projects</a></li>
+                <li><a href='./contact/'>Contact Me</a></li>
+              </ul>
+            </div>
+          </div>";
+}
+
+?>
---
+++ css/index.php
@@ -0,0 +1,4 @@
+<?PHP
+include("../config.php");
+header('Location: ' . $home . '');
+?>
